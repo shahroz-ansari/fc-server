727a8ed04774bb20da4debcb721f61a1ae6bf1bc
shahroz-dew <mohammads@dewsolutions.in>
2020-12-20 00:35:02 +0530

    setup local pouchdb server for dev env

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var pouchHandle = require('./config/express-pouch')
 
 var app = express();
 
@@ -10,5 +11,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+if (process.env.ENVIRONMENT === 'development') {
+    app.use('/', pouchHandle);
+}
 
 module.exports = app;
