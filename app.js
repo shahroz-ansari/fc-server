9131e272d9d17083e4e9b675296dff23b056766b
shahroz-dew <mohammads@dewsolutions.in>
2020-12-23 22:51:49 +0530

    removed express pouch dev enviroment

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var pouchHandle = require('./config/express-pouch')
 require('./config/mongoose')();
 
 var appRoutes = require('./routes');
@@ -16,9 +15,5 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/fc', appRoutes)
-
-if (process.env.ENVIRONMENT === 'development') {
-    app.use('/', pouchHandle);
-}
 
 module.exports = app;
