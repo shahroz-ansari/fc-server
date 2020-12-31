e477baf13c34e611d57e10c491979424dfc4f410
shahroz-dew <mohammads@dewsolutions.in>
2020-12-31 15:42:56 +0530

    added addFcUser functionality

--- services/fcUsers/index.js
+++ services/fcUsers/index.js
@@ -6,9 +6,34 @@ class FcUsersDB extends CouchDB {
         super(dbName)
     }
 
-    addNewUser(user) {
-        // validate user data
-        return this.put(`${user.userFcId}:${v4()}`, user);
+    // @user should contain firstName or lastName
+    addFcUser(userFcId, user) {
+        return new Promise(async (resolve, reject) => {
+            if (!userFcId) {
+                reject(new Error('userFcId is required.'))
+                return;
+            }
+
+            if (!(user.firstName || user.lastName)) {
+                reject(new Error('firstName or lastName is required.'))
+                return;
+            }
+
+            const fcUser = {
+                firstName: user.firstName || '',
+                lastName: user.lastName || '',
+                picture: user.picture || '',
+                connections: [userFcId]
+            }
+
+            const resp = await this.put(`/fcuser::${userFcId}`, fcUser);
+            
+            if (resp.error) {
+                reject(new Error(resp.error));
+                return;
+            }
+            resolve(resp);
+        })
     }
 }
 
