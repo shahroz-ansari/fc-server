6782dc0bb9db51e66f0a065fcf70ee774deb60a0
shahroz-dew <mohammads@dewsolutions.in>
2021-01-02 22:22:15 +0530

    added validation on doc update

--- config/db/fcGroups/design_doc.js
+++ config/db/fcGroups/design_doc.js
@@ -4,9 +4,61 @@ module.exports = [
         "language": "javascript",
         "validate_doc_update": String(
             function(newDoc, oldDoc, userCtx) {
-                // only server admin can write
-                if (userCtx.roles.indexOf('_admin') !== -1) { return; }
-                else { throw ({ forbidden: 'Unauthorized Access' });}
+                if (!newDoc.title) {
+                    throw({forbidden: 'title is required'});
+                }
+
+                if (!newDoc.adminId) {
+                    throw({forbidden: 'adminId is required'});
+                }
+
+                if (!Array.isArray(newDoc.users)) {
+                    throw({forbidden: 'users is required'});
+                }
+
+                // if duplicates exist in users
+                if (newDoc.users.some( function(e,i,a) { return a.indexOf(e) !== i}) ) {
+                    throw ({ forbidden: 'duplicate users not allowed' })
+                }
+
+                // if oldDoc not exist treat newDoc as oldDoc
+                const doc = oldDoc || newDoc
+
+                // can't transfer admin rights to other user
+                if (newDoc.adminId !== doc.adminId) {
+                    throw({forbidden: 'can\'t transfer admin rights'});
+                }
+
+                if (newDoc.users.indexOf(doc.adminId) < 0) {
+                    throw({forbidden: 'users must contain admin id'});
+                }
+
+                // server admin can perform any action
+                if (userCtx.roles.indexOf('_admin') !== -1){
+                    return;
+ 
+                // if user is group admin
+                } else if (userCtx.name === doc.adminId) {
+                    return;
+
+                // if user is member of group
+                } else if (doc.users.indexOf(userCtx.name) > -1) {
+                    // can't delete group
+                    if (newDoc._deleted === true) {
+                        throw({forbidden: 'only admin may delete group'});
+                    }
+
+                    // can't modify group members
+                    if (
+                        !doc.users.every(function(element) { return newDoc.users.indexOf(element) > -1 }) ||
+                        doc.users.length !== newDoc.users.length
+                    ) {
+                        throw ({ forbidden: 'only group admin can add members'})
+                    }
+
+                } else {
+                    throw ({ forbidden: 'only group admin and members can write'})
+                }
             }
         )
     },
--- config/db/fcUsers/design_doc.js
+++ config/db/fcUsers/design_doc.js
@@ -4,9 +4,102 @@ module.exports = [
         "language": "javascript",
         "validate_doc_update": String(
             function(newDoc, oldDoc, userCtx) {
-                // only server admin can write
-                if (userCtx.roles.indexOf('_admin') !== -1) { return; }
-                else { throw ({ forbidden: 'Unauthorized Access' });}
+
+                if (!newDoc.userFcId) {
+                    throw ({forbidden: 'userFcId is required'});
+                }
+
+                if (!newDoc.firstName && !newDoc.lastName) {
+                    throw ({forbidden: 'firstName or lastName is required'});
+                }
+
+                const doc = oldDoc || newDoc;
+
+                // userFcId can't be modified
+                if (newDoc.userFcId !== doc.userFcId) {
+                    throw({forbidden: 'can\'t update userFcId'});
+                }
+
+                // connection must contain his own id
+                if (newDoc.connections.indexOf(doc.userFcId) < 0) {
+                    throw ({forbidden: 'connections must contain fcUserId'});
+                }
+
+                // duplicate connections not allowed
+                if (newDoc.connections.some( function(e,i,a) { return a.indexOf(e) !== i}) ) {
+                    throw ({ forbidden: 'duplicate connecitons not allowed' })
+                }
+
+                // server admin can perform any action
+                if (userCtx.roles.indexOf('_admin') !== -1) { 
+                    return;
+                
+                }
+                
+                // users other than admin can't modify these fields
+                if (
+                    newDoc._deleted === true
+                ) { 
+                    throw ({ forbidden: 'only admin can delete' });
+                }
+
+                if (
+                    (
+                        newDoc.firstName !== doc.firstName ||
+                        newDoc.lastName !== doc.lastName ||
+                        newDoc.picture !== doc.picture
+                    ) &&
+                    userCtx.name !== doc.userFcId
+                ) {
+                    throw ({ forbidden: 'only doc owner can update firstName, lastName and picture'})
+                }
+
+                // Inserting his fcId
+                if (newDoc.connections.length === doc.connections.length + 1) {
+                    // make sure not already present
+                    if (doc.connections.indexOf(userCtx.name) > -1) {
+                        throw ({ forbidden: 'fcId already exist in connections' });
+                    }
+
+                    // old ids should be present new newDoc
+                    doc.connections.forEach(function(element){
+                        if (newDoc.connections.indexOf(element) < 0) {
+                            throw ({ forbidden: 'can\'t change other fcIds' });
+                        }
+                    });
+
+                    // validate id inserted is his own
+                    if (newDoc.connections.indexOf(userCtx.name) < 0) {
+                        throw ({ forbidden: 'can\'t change other fcIds' })
+                    }
+
+                // deleting his fcId
+                } else if (newDoc.connections.length === doc.connections.length - 1) {
+                    // make sure his id exist
+                    if (doc.connections.indexOf(userCtx.name) < 0) {
+                        throw ({ forbidden: 'fcId not exists in connecitons' });
+                    }
+
+                    // old ids should be present new newDoc accept his own
+                    doc.connections.forEach(function(element){
+                        if (element !== userCtx.name && newDoc.connections.indexOf(element) < 0) {
+                            throw ({ forbidden: 'can\'t change other fcIds' });
+                        }
+                    });
+
+                // nothing altered
+                } else if (newDoc.connections.length === doc.connections.length) {
+                    // all fcIds should be there as it is
+                    doc.connections.forEach(function(element){
+                        if (newDoc.connections.indexOf(element) < 0) {
+                            throw ({ forbidden: 'can\'t change other fcIds' });
+                        }
+                    });
+
+                // any other operation on connections
+                } else {
+                    throw ({ forbidden: 'access_denied'})
+                }
             }
         )
     },
